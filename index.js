3076fb725209a45371c41cd3c6be427224f388ee
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-12 10:17:05 +0300

    Example rabbit

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
-"use strict";
+// "use strict";
 
-function toString() {
-  return `${this.name} ${this.sname}`;
-}
+// function toString() {
+//   return `${this.name} ${this.sname}`;
+// }
 
-const animal = {
-  name: "anonimous",
-  eat: function () {
-    return this.name + "is eating";
-  },
-};
-const man = {
-  countFingers: 5,
-  name: "human",
-  read: function () {
-    return this.name + "is reading";
-  },
-};
-const student = {
-  id: 1,
-  name: "Elon",
-  sname: "Musk",
-  age: 50,
-  toString,
-};
-
-man.__proto__ = animal;
-student.__proto__ = man;
-
-console.log(student);
-console.log(student.read());
-console.log(man.eat());
-
-// const student2 = {
-//   id: 2,
-//   name: "Bob",
-//   sname: "Rob",
+// const animal = {
+//   name: "anonimous",
+//   eat: function () {
+//     return this.name + "is eating";
+//   },
+// };
+// const man = {
+//   countFingers: 5,
+//   name: "human",
+//   read: function () {
+//     return this.name + "is reading";
+//   },
+// };
+// const student = {
+//   id: 1,
+//   name: "Elon",
+//   sname: "Musk",
 //   age: 50,
-//   toString: toString,
+//   toString,
 // };
 
+// man.__proto__ = animal;
+// student.__proto__ = man;
+
 // console.log(student);
-// console.log(student.toString());
-// console.log(student.toQwerty); //undefined
-// console.log(student2);
-// console.log(student2.toString());
-// console.log(student2.toString === student.toString);
+// console.log(student.read());
+// console.log(man.eat());
+
+//-----------------------------------//
+
+"use strict";
+
+const rabbit = {
+  countPaw: 4,
+  jump: function () {
+    return "i\'m jumping";
+  },
+};
+
+const rabbitPet = {
+  name:'Boba',
+  pet: function () {
+    return 'pet' + this.name;
+  },
+};
+
+const rabbitMagic = {
+  say: function (Hello) {
+    return "word";
+  },
+};
+
+rabbitPet.__proto__ = rabbit;
+rabbitMagic.__proto__ = rabbitPet;
+
+
