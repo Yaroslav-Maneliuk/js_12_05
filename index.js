ac8141012c8606dfa0a5cd5b601b0808a541c33c
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-12 09:51:10 +0300

    Example __proto__

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 "use strict";
+
+function toString() {
+  return `${this.name} ${this.sname}`;
+}
+
+const animal = {
+  name: "anonimous",
+  eat: function () {
+    return this.name + "is eating";
+  },
+};
+const man = {
+  countFingers: 5,
+  name: "human",
+  read: function () {
+    return this.name + "is reading";
+  },
+};
+const student = {
+  id: 1,
+  name: "Elon",
+  sname: "Musk",
+  age: 50,
+  toString,
+};
+
+man.__proto__ = animal;
+student.__proto__ = man;
+
+console.log(student);
+console.log(student.read());
+console.log(man.eat());
+
+// const student2 = {
+//   id: 2,
+//   name: "Bob",
+//   sname: "Rob",
+//   age: 50,
+//   toString: toString,
+// };
+
+// console.log(student);
+// console.log(student.toString());
+// console.log(student.toQwerty); //undefined
+// console.log(student2);
+// console.log(student2.toString());
+// console.log(student2.toString === student.toString);
