78254629267921929a0feb9d3652a1b6589c2735
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-17 12:08:50 +0300

    Example Hard(3var)

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 "use strict";
 
-const num = new Number(10);
-console.log(num);
+//----------------1var-----------------------------//
+/*
+function string() {
+  let string = "tO be OR Not TO bE";
+  let splits = string.split(" ");
+  let stringFinal = "";
 
-const num2 = Number(123.123123.toFixed(2));
-console.log(num2);
+  for (let i = 0; i < splits.length; i++) {
+    let Name = splits[i];
+    let First = Name.substring(0, 1).toUpperCase();
+    let Leftovers = Name.substring(1).toLowerCase();
+    stringFinal += First + Leftovers + " ";
+  }
+  console.log(stringFinal);
+}
+string();
+*/
+//----------------2var-----------------------------//
+/*
+function splitString(string, separator) {
+  const arrayOfStrings = string.split(separator);
+  for(let i = 0; i < arrayOfStrings.length; i++) {
+    arrayOfStrings[i] = arrayOfStrings[i].substr(0,1).toUpperCase() + arrayOfStrings[i].substr(1).toLowerCase();
+}
+return arrayOfStrings.join(separator);
+}
+const tempestString = 'sadsadsa dsadas das dss adsa sa sa sad asd ';
+const space = ' ';
+console.log(splitString(tempestString, space));
+*/
+//----------------NIKITA -var----------------------//
 
+function stringToUpper(string) {
+  return string
+    .split(" ")
+    .map(function (element) {
+      return element[0].toUpperCase() + element.substring(1).toLowerCase();
+    })
+    .join(" ");
+}
+console.log(stringToUpper("tO be OR Not TO bE"));
 
-let number = 50.2561;
-number = Number(number.toFixed(2))
-console.log(number);
+//----------------OLENA-var----------------------//
