f81376845f5269aafb7edae87f3cbdc58fee68a6
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-17 09:36:43 +0300

    "use strict" //this

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
 // };
 
 const getMaxFromTwoNumber = (n1, n1) => n1 > n2 ? n1 : n2;
+
+const showArgs = (a, b, ...c) => {
+  console.log(c)
+  c.forEach((item)=>{
+    console.log(item)
+  })
+};
+showArgs(1,2,3,4,5);
+
+const obj = {
+  prop:12,
+  showArgs,
+}
+obj.showArgs(1,1,1,1);
