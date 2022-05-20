cba52297378a0de4171b20468c58b1c4c8a2dade
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-20 09:14:51 +0300

    Example add class

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-'use strict'
+"use strict";
+// class Test{};
+// const Test = class{};
 
+
+function test(a, b, ...rest) {
+  return class {
+    constructor(prop) {
+      this.prop = prop;
+    }
+  };
+}
+const TestClass = test(1,2,3,4)
+
+const obj = new TestClass(4);
+
+console.dir(TestClass);
