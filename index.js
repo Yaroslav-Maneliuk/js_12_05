1a6f860a910519e0a7e9c11c5484f1e69d4f3ff5
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-30 11:26:14 +0300

    Add compare v1.0

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
 "use strict";
 
-const user1 = {
-  id: 1,
-  name: "Elon",
-  age: 50,
-};
+/*
+написать фунцию, которая проверяет может ли одна строка юыть создана из символов другой строки
 
-const user2 = {
-  id: 2,
-  name: "Elen",
-  age: 40,
-};
+compare('test', 'sett'); //true
+compare('love', 'move'); //false
 
-const msgElon = ["has", "already", "been", "declared"];
-const msgElen = ["Uncaught", "SyntaxError", "Unexpected", "Hi!"];
+1)создать мапу для каждого слова
+  -для каждого символа
+    -проверять существование ключа(буквы)
+      -если есть, то увеличиваем значение
+      -если нет, то устанавливаем новую пару ключ:значение(1)
+2)сравниваем размер мапы:
+  -если по размеру разные. значит false
+3) если по размеру одинкаовые:
+  -проходимся по ключам одной мапы и сравниваем знасчение у соответствующих ключей второй мапы
+*/
 
-const usersMeassage = new Map();
-usersMeassage.set(user1, msgElon);
-usersMeassage.set(user2, msgElen);
-
-function getMessage(user) {
-  if (usersMeassage.has(user)) {
-    usersMeassage.get(user).forEach((message) => {
-      console.log(message);
-    });
+function compare(str1, str2) {
+  const mapStr1 = new Map();
+  for (const letter of str1) {
+    //letter->key
+    if (mapStr1.has(letter)) {
+      const value = mapStr1.get(letter);
+      mapStr1.set(letter, value + 1);
+    } else {
+      mapStr1.set(letter, 1);
+    }
   }
-  // return usersMeassage.get(user);
+  const mapStr2 = new Map();
+  for (const letter of str2) {
+    //letter->key
+    if (mapStr2.has(letter)) {
+      const value = mapStr2.get(letter);
+      mapStr2.set(letter, value + 1);
+    } else {
+      mapStr2.set(letter, 1);
+    }
+  }
+  console.log(mapStr1)
+  console.log(mapStr2)
+  if (mapStr1.size !== mapStr2.size) {
+    return false;
+  }
+  for (const letter of mapStr1.keys()) {
+    if(mapStr1.get(letter) !== mapStr2.get(letter)){
+      return false;
+    }
+  }
+  return true;
 }
 
-for (const key of usersMeassage.keys()) {
-  console.log(key.name, 'says:')
-  getMessage(key);
-}
-
-getMessage(user1)
+console.log(compare("test", "sett"));
+console.log(compare("love", "move"));
+console.log(compare("qwerty", "qwer"));
